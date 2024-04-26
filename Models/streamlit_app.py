da11008a80683cac395dd80fa5fd68b023ecd6ab
Sai Ram Manikanta <sairammanikanta.danduboina@thomsonreuters.com>
2024-04-26 07:22:59 +0000

    Google Gen AI configured

--- Models/streamlit_app.py
+++ Models/streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import google.generativeai as genai
 
-st.title('Hello, Streamlit!')
+GOOGLE_API_KEY = 'AIzaSyBrWhFNp-QDj3yAjJbW7-ukgqoRYO2-vL4'
 
-text = st.text_area('Enter some text')
-submit = st.button('Generate SQL Query')
+genai.configure(api_key = GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+
+st.title('LLM Model Draft v1.0')
+
+text_input = st.text_area('Ask query here:')
+submit = st.button('Generate Response')
+
+if submit:
+    response = model.generate_content(text_input)
+    st.write(response.text)
